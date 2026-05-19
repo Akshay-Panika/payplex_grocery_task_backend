b16f8c6eada2f6ad3a95722d371ea8853a674d64
akshe <akshey.panika@gmail.com>
2026-05-20 01:27:45 +0530

    order update

--- orders/models.py
+++ orders/models.py
@@ -4,6 +4,7 @@ class Order(models.Model):
     order_id = models.CharField(max_length=20, unique=True, blank=True)
     items = models.JSONField()  # example: [1,2,3]
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
